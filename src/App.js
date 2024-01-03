7b1ea3ce6408c824568b785df2de46c3fc78a6a6
kaushal5504 <kaushalpatel0504@gmail.com>
2024-01-03 19:43:13 +0530

    project ready

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tour from "./components/Tour";
+
+
+function App() {
+  return (
+   
+    <>
+    <Navbar/>
+    <Hero/>
+    <Services/>
+    <About/>
+    <Tour/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const dataLinks = [
+    {id:1, text: "home", href: "#home"},
+    {id:2, text: "about", href: "#about"},
+    {id:3, text: "services", href: "#services"},
+    {id:4, text: "tours", href: "#tours"},
+]
+
+export const socialLinks = [
+    {id:1, href: "https://www.twitter.com", icon: "fab fa-facebook"},
+    {id:2, href: "https://www.twitter.com", icon: "fab fa-twitter"},
+    {id:3, href: "https://www.twitter.com", icon: "fab fa-squarespace"},
+]
+export const services=[
+    {id:1 , icon:'fas fa-wallet fa-fw' , title:'saving money' , text:'lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
+    {id:2, icon:'fas fa-tree fa-fw' , title:'endless hiking' , text:'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.'},
+    {id:3 , icon:'fas fa-socks fa-fw' , title:'amazing comfort' , text:'lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
+    
+]
+
+export const tours=[
+    {   id:1, 
+        img:'./images/tour-1.jpeg', 
+        date:'august 26th, 2020', 
+        
+        title:'Tibet Adventure', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        country:'china', 
+        days:'6 days', 
+        amount:'from $2100'
+    },
+
+    {id:2, img:'./images/tour-2.jpeg', date:'august 26th, 2020', title:'Tibet Adventure', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', country:'', days:'6 days', amount:'from $2100'},
+
+    {id:3, img:'./images/tour-3.jpeg', date:'august 26th, 2020', title:'Tibet Adventure', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', country:'', days:'6 days', amount:'from $2100'},
+
+    {id:4, img:'./images/tour-4.jpeg', date:'august 26th, 2020', title:'Tibet Adventure', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', country:'', days:'6 days', amount:'from $2100'},
+
+    {id:5, img:'./images/tour-5.jpeg', date:'august 26th, 2020', title:'Tibet Adventure', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', country:'', days:'6 days', amount:'from $2100'},
+
+    {id:6, img:'./images/tour-6.jpeg', date:'august 26th, 2020', title:'Tibet Adventure', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.', country:'', days:'6 days', amount:'from $2100'},
+]
